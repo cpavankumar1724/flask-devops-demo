584d1c1315f681eececfcba2028767a99f865060
root <root@jenkins-server.asia-south1-c.c.phoenix-devops-lab.internal>
2026-07-14 13:20:23 +0000

    Updated application to version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello Mava! 🚀 Jenkins + Docker Working Successfully!</h1>"
+    return "<h1>Hello Mava! 🚀Version -2 is successfully deployed in Jenkins Pipeline!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
